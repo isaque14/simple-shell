260c89f041640d25e87b8c76f33e4e2195c93596
Nicolas Magalhães <magalhaesnicolas23@gmail.com>
2021-10-04 16:00:14 -0300

    Added custom commands (cd , exit and help)

--- bash.c
+++ bash.c
@@ -26,22 +26,35 @@ void help()
     printf("\n\n\n**********************************\n");
     printf("\n\n\n        MENU DE AJUDA\n");
     printf("\n\n COMANDOS:");
-    printf("\n Qualquer comando UNIX shell");
+    printf("\n- cd");
+    printf("\n- exit");
+    printf("\n- help");
+    printf("\n- Qualquer comando UNIX shell");
 
     printf("\n\n\n***********************************\n");
 }
 
 void showDir() {
-
-    char currentDir[SIZE];                   
+   // char* user = getenv("USER");
+    char* currentDir;
+    if( (currentDir = getcwd(NULL, 0)) == NULL) {
+        perror("failed to get current directory\n");
+    }           
     getcwd(currentDir, sizeof(currentDir));
-    printf("\n Current Directory: %s", currentDir);
+    //char* dir = strcat(user,"@");
+    //dir = strcat(dir,currentDir);
+
+    printf("\n%s@%s ",getenv("USER"),currentDir);
+    free(currentDir);
 }
+
 
 int inputManager(char* input){
 
     char* temp;
-    temp = readline("\n >>> ");
+    //char* dir = showDir();
+   // printf("\n%s ",dir);
+    temp = readline(">>> ");
     if(strlen(temp) != 0){
         add_history(temp);
         strcpy(input, temp);
@@ -49,6 +62,35 @@ int inputManager(char* input){
     }
     else
         return 1;
+}
+int customCmdManager(char** input){
+
+    int cmdTotal = 3;
+    char* cmdList[cmdTotal];
+    int cmd = 0;
+    cmdList[0] = "exit";
+    cmdList[1] = "help";
+    cmdList[2] = "cd";
+    
+    for(int i = 0; i < cmdTotal; i++){
+        if(strcmp(input[0], cmdList[i]) == 0){
+            cmd = i+1;
+        }
+    }
+
+    switch(cmd){
+        case 1:
+            exit(0);
+        case 2:
+            help();
+            return 1;
+        case 3:
+            chdir(input[1]);
+            return 1;
+        default:
+            break;
+    }
+    return 0;
 }
 void spaceInput(char* input, char** wordlist){
     for(int i = 0; i< LIST; i++){
@@ -65,6 +107,9 @@ void spaceInput(char* input, char** wordlist){
 int parseInput(char* input,  char** wordlist){
 
     spaceInput(input,wordlist);
+
+    if(customCmdManager(wordlist))
+        return 0;
     return 1;
 }
 
@@ -87,11 +132,15 @@ void execCommands(char** wordlist){
     }
 }
 
+
+
 int main(void){
     char input[WORD], *wordlist[LIST];
     int execflag = 0;
     startup();
     while(1){
+        showDir();
+    
         if(inputManager(input))
             continue;
 
